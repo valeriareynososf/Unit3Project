fa9250ec7009ac48258dd8b5e5fe4b51467a8608
Valeria Reynoso <valeriayearup@gmail.com>
2023-09-12 23:17:01 -0700

    step 4

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 console.log("test");
+const name = document.querySelector('#name').focus();
+const jobRole = document.querySelector("#title");
+const otherJobRole = document.querySelector("#other-job-role");
+otherJobRole.type ="hidden"
+console.log("jobRole :",jobRole.value);
+jobRole.addEventListener('change', (e)=>{
+    if (e.target.value === "other") {
+        console.log("e",e.target.value);
+        otherJobRole.type ="text"
+    } else {
+        console.log("NO",e.target.value);
+        otherJobRole.type ="hidden"
+    }
+    console.log("e",e.target.value);
+})
